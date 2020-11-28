ca185d931e0c6789420a87ebfcd734979dd70857
RAJATsd <rajatsharmaatri@gmail.com>
2020-11-28 23:52:37 +0530

    done with cart toggle element and used reslect to save rerender

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import CustomButton from '../custom-button/custom-button.component';
-
 import './cart-dropdown.styles.scss';
+import {connect} from 'react-redux';
 
-const CartDropdown = () => (
+import CartItem from '../cart-item/cart-item.component';
+import CustomButton from '../custom-button/custom-button.component';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
+
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+        {
+            cartItems.map((item)=>(
+                <CartItem 
+                    key={item.id} 
+                    item={item} 
+                />
+            ))
+        }
+        </div> 
         <CustomButton>CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+    cartItems:selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropdown);
