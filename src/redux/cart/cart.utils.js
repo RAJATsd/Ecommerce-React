3d520d213998c6e92f9e362791acfa8fae276eab
RAJATsd <rajatsharmaatri@gmail.com>
2020-11-30 14:42:49 +0530

    added addition or removing of items from cart by the help of arrows clicking

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,19 @@ export const addItemsToCart = (cartItems,cartItemToAdd) => {
     
     return [...cartItems,{...cartItemToAdd,quantity:1}]
 }
+
+export const removeItemFromCart = (cartItems,itemToRemove) => {
+    const existingCartItem = cartItems.find(
+        item=>item.id===itemToRemove.id
+    ); 
+    if(existingCartItem.quantity===1)
+    {
+       return cartItems.filter(item => item.id !== itemToRemove.id);
+    }
+    return cartItems.map(item => (
+        item.id===itemToRemove.id ?
+            {...item,quantity:item.quantity - 1}
+            :
+            item
+    ))
+}
